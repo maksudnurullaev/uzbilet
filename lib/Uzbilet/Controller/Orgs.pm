0d4092ad53c9361f32b3bd4db78cf5ea940aed45
maksudnurullaev <maksud.nurullaev@gmail.com>
2017-04-28 15:21:29 +0500

    Done org->add functionality

--- lib/Uzbilet/Controller/Orgs.pm
+++ lib/Uzbilet/Controller/Orgs.pm
@@ -2,6 +2,7 @@ package Uzbilet::Controller::Orgs; {
 use Mojo::Base 'Mojolicious::Controller';
 
 use Data::Dumper;
+use Db;
 
 sub add{
     my $self = shift;
@@ -14,12 +15,22 @@ sub add{
     $validation->optional('description','trim');
 
     if( $validation->has_error ){
-        say for @{$validation->failed};
+        #TODO remove late - say for @{$validation->failed};
 		$self->stash(formWithError => 1);
+		$self->stash(internalError => 1);
         return;
     }
 
-	say Dumper $self->req->params->to_hash ;
+	my $dbh = Db->new($self);
+	
+	my $new_record = $self->req->params->to_hash;
+	$new_record->{object_name} = 'organization';
+    
+	if( $dbh->insert($new_record) ){
+		$self->redirect_to("/orgs/");
+	} else {
+		$self->stash(internalError => 1);
+	}
 };
 
 1;
