ab13f85dd6cb79048ce35a251d9bdaf26180265e
maksudnurullaev <maksud.nurullaev@gmail.com>
2017-05-11 18:41:25 +0500

    Minor changes

--- lib/Uzbilet/Controller/Orgs.pm
+++ lib/Uzbilet/Controller/Orgs.pm
@@ -7,28 +7,23 @@ use Utils::Auth;
 
 sub start{
     my $self = shift;
-	return if !Utils::Auth::has_role($self,'admin');
 
+	return if !Utils::Auth::has_role($self,'admin');
     $self->stash(orgs => Utils::Orgs::get_many($self));
 };
 
 sub add{
     my $self = shift;
-	return if !Utils::Auth::has_role($self,'admin');
-    return if $self->req->method ne 'POST' ;
 
-    Utils::Orgs::add($self) if !Utils::Orgs::has_error($self);
+	return if !Utils::Auth::has_role($self,'admin');
+    Utils::Orgs::add($self) if $self->req->method eq 'POST' && !Utils::Orgs::has_error($self);
 };
   
 sub edit{
     my $self = shift;
-	return if !Utils::Auth::has_role($self,'admin');
-    if( $self->req->method ne 'POST' ){
-        $self->stash(object => Utils::Orgs::get_1($self, $self->param('payload')));
-        return;
-    };
 
-    Utils::Orgs::update($self) if !Utils::Orgs::has_error($self) ;
+    return if !Utils::Auth::has_role($self,'admin');
+    Utils::Orgs::update($self) if $self->req->method eq 'POST' && !Utils::Orgs::has_error($self);
     $self->stash(object => Utils::Orgs::get_1($self, $self->param('payload')));
 };
 
